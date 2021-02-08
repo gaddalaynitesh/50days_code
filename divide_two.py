fd408391e0460d8a7a5e1e81b48e5f26e2d3187a
gaddalaynitesh <34005937+gaddalaynitesh@users.noreply.github.com>
2021-02-08 08:42:01 -0600

    Create divide_two.py
    problem statment 
    https://leetcode.com/problems/divide-two-integers/submissions/

---
+++ divide_two.py
@@ -0,0 +1,12 @@
+class Solution:
+    def divide(self, dividend: int, divisor: int) -> int:
+        if dividend == -2147483648 and divisor == -1:
+            return 2147483647
+        elif dividend == -2147483648 and divisor == 1:
+            return -2147483648
+        elif dividend // divisor >= 0:
+            return dividend // divisor
+        else:
+            if (dividend / divisor) == int (dividend / divisor):
+                return dividend // divisor
+            return dividend // divisor +1
